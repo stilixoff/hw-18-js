ffa9aa398afdd0b2a71762343c0539331f4e325a
Oleh Starodubet <stilixoff@gmail.com>
2020-04-13 15:34:56 +0300

    Updates

---
+++ precache-manifest.dc99f6020258b99af517ba589f80b5e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062ed3f87bd76bcab0f0b81fe8373040",
+    "url": "/hw-18-js/index.html"
+  },
+  {
+    "revision": "6585caed12eaf9c47b2f",
+    "url": "/hw-18-js/static/css/main.3cf174a0.chunk.css"
+  },
+  {
+    "revision": "d80001ac5837846ba24a",
+    "url": "/hw-18-js/static/js/2.8e04fd5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw-18-js/static/js/2.8e04fd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6585caed12eaf9c47b2f",
+    "url": "/hw-18-js/static/js/main.bd8306f3.chunk.js"
+  },
+  {
+    "revision": "40af70f5e9e6dc22d008",
+    "url": "/hw-18-js/static/js/runtime-main.046c0191.js"
+  },
+  {
+    "revision": "1fa03c0b4e11016b75ef770332349c22",
+    "url": "/hw-18-js/static/media/ramka.1fa03c0b.png"
+  }
+]);
